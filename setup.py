eba7c2811b05e880d0715c587b96e972afc1c4dd
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-09-16 13:41:02 -0400

    init

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+requirements = [
+    "funcy",
+    "argparse",
+    "pefile",
+    "ipython",
+    "vivisect"
+]
+
+setup(
+    name='viv_utils',
+    version='0.1.0',
+    description="Utilities for binary analysis using vivisect.",
+    long_description="Utilities for binary analysis using vivisect.",
+    author="Willi Ballenthin",
+    author_email='william.ballenthin@mandiant.com',
+    url='https://github.mandiant.com/wballenthin/viv-utils',
+    packages=[
+        'viv_utils',
+    ],
+    package_dir={'viv_utils':'viv_utils'},
+    package_data={'viv_utils': ['data/*.py']},
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='viv_utils',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
