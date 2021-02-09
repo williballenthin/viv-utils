e56e6ceb720696137e16e1598bff3e60df3e8491
Willi Ballenthin <willi.ballenthin@gmail.com>
2021-02-09 08:14:17 -0700

    setup: v0.3.19

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setuptools.setup(
     name='viv_utils',
-    version='0.3.18',
+    version='0.3.19',
     description="Utilities for binary analysis using vivisect.",
     long_description="Utilities for binary analysis using vivisect.",
     author="Willi Ballenthin",
