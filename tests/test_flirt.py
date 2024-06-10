de6bacb5590aab7f93127d218ae80dd2bc3bbee6
Soufiane Fariss <rev.fariss@gmail.com>
2024-06-10 14:17:55 +0200

    move callgraph into its respective testcase

--- tests/test_flirt.py
+++ tests/test_flirt.py
@@ -23,26 +23,30 @@ def test_function_not_called(sample_038476):
     assert is_only_called_from_library_functions(sample_038476, 0x408155) is False
 
 
-# call graph of 0x40CAA3:
-#
-#     entry (0x4081155)
-#              |
-#              v
-#       FUN_407660   FUN_403520
-#              |        |
-#              v        v
-#       FUN_4027d0   FUN_404980
-#              \        /
-#               \      /
-#                v    v
-#              FUN_40ca76
-#                  |
-#                  v
-#              FUN_40caa3
-
-
 def test_only_called_from_library_functions(sample_038476):
     # this is a function whose top level callers are *all* library functions
+
+    # call graph of 0x40CAA3:
+    #
+    # Note:
+    #   - recognized library functions are denoted by "(lib)"
+    #   - non-library functions have no designation
+    #
+    #      entry (0x4081155) <<< (lib)
+    #              |
+    #              v
+    #       FUN_407660   FUN_403520 <<< (lib)
+    #              |        |
+    #              v        v
+    #       FUN_4027D0   FUN_404980
+    #              \        /
+    #               \      /
+    #                v    v
+    #              FUN_40CA76
+    #                  |
+    #                  v
+    #              FUN_40CAA3
+
     func_addr = 0x40CAA3
     top_level_func_addr1 = 0x408155
     top_level_func_addr2 = 0x403520
@@ -60,6 +64,28 @@ def test_only_called_from_library_functions(sample_038476):
 
 def test_called_from_mixed_functions(sample_038476):
     # this is a function whose top level callers are both library and non-library functions
+
+    # call graph of 0x40CAA3:
+    #
+    # Note:
+    #   - recognized library functions are denoted by "(lib)"
+    #   - non-library functions have no designation
+    #
+    #       entry (0x4081155) <<< (lib)
+    #              |
+    #              v
+    #       FUN_407660   FUN_403520
+    #              |        |
+    #              v        v
+    #       FUN_4027D0   FUN_404980
+    #              \        /
+    #               \      /
+    #                v    v
+    #              FUN_40CA76
+    #                  |
+    #                  v
+    #              FUN_40CAA3
+
     func_addr = 0x40CAA3
     top_level_func_addr = 0x408155  # parent caller of 0x40CAA3
 
@@ -71,27 +97,31 @@ def test_called_from_mixed_functions(sample_038476):
     assert is_only_called_from_library_functions(sample_038476, func_addr) is True
 
 
-# call graph of 0x40B06C:
-#
-#       FUN_407BF0 <--- FUN_407B3C
-#           |              ^
-#           v              |
-#       FUN_408294         |
-#           |              |
-#           v              |
-#       FUN_40832F ---> FUN_4084D6
-#                 \     /
-#                  \   /
-#                   v v
-#              FUN_40868F
-#                    |
-#                    v
-#               FUN_408840
-#                    |
-#                    v
-#               FUN_40b06C
-
-
 def test_function_circular_call_graph(sample_038476):
     # this is a function whose call graph contains a cycle
+
+    # call graph of 0x40B06C:
+    #
+    # Note:
+    #   - recognized library functions are denoted by "(lib)"
+    #   - non-library functions have no designation
+    #
+    #       FUN_407BF0 <--- FUN_407B3C
+    #           |              ^
+    #           v              |
+    #       FUN_408294         |
+    #           |              |
+    #           v              |
+    #       FUN_40832F ---> FUN_4084D6
+    #                 \     /
+    #                  \   /
+    #                   v v
+    #              FUN_40868F
+    #                    |
+    #                    v
+    #               FUN_408840
+    #                    |
+    #                    v
+    #               FUN_40B06C
+
     assert is_only_called_from_library_functions(sample_038476, 0x40B06C) is False
